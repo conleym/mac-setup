9e24691798f0b7023eb48c5812b16a0e429c4941
Mike Conley <conleym@users.noreply.github.com>
2020-05-17 15:31:52 -0400

    Give setup.sh the full path to ansible-playbook.
    Python user scripts dir isn't on the path, obviously.

--- setup.sh
+++ setup.sh
@@ -3,10 +3,12 @@
 SCRIPT="${(%):-%x}"
 DIR="$( cd "$( dirname "${SCRIPT}" )" >/dev/null 2>&1 && pwd )"
 
+
 # Make sure we pick up macports once it's installed.
 # When we install macports, we add to /etc/paths. But the shell running now
 # might not have that set up yet, causing failures later. Ensure path is
 # defined here.
 export PATH=/opt/local/bin:/opt/local/sbin:"${PATH}"
 
-ansible-playbook "${DIR}"/setup-playbook.yml "$@"
+# Assumes ansible was installed with pip3 --user (see init.sh).
+"$(python3 -m site --user-base)"/bin/ansible-playbook "${DIR}"/setup-playbook.yml "$@"
