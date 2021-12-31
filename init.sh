31666f79af280377eddb54e287c1594911ab38f3
Mike Conley <conleym@users.noreply.github.com>
2021-12-31 13:36:09 -0500

    Add ansible galaxy requirements to init.

--- init.sh
+++ init.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env zsh
 
+SCRIPT="${(%):-%x}"
+DIR="$( cd "$( dirname "${SCRIPT}" )" >/dev/null 2>&1 && pwd )"
+
 # Accept the Xcode license
 sudo xcodebuild -license accept
 
 # Install the latest version of ansible
 pip3 install --user --upgrade ansible
+
+# Install playbook requirements.
+"$(python3 -m site --user-base)"/bin/ansible-galaxy install -r "${DIR}"/requirements.yml
