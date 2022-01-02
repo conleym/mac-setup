ac8b8afb8c22310f7058db0c8af8d604221a958c
Mike Conley <conleym@users.noreply.github.com>
2022-01-01 21:42:30 -0500

    Better bootstrapping in init. Start to support Big Sur and beyond.

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+SCRIPT="${(%):-%x}"
+DIR="$( cd "$( dirname "${SCRIPT}" )" >/dev/null 2>&1 && pwd )"
+
+# Run bootstrap. Installs Xcode's "additional components", if necessary. Installs MacPorts and ports required by setup.
+"$(python3 -m site --user-base)"/bin/ansible-playbook "${DIR}"/bootstrap-playbook.yml "$@"
--- init.sh
+++ init.sh
@@ -3,14 +3,15 @@
 SCRIPT="${(%):-%x}"
 DIR="$( cd "$( dirname "${SCRIPT}" )" >/dev/null 2>&1 && pwd )"
 
-# Accept the Xcode license
+# Accept the Xcode license. Can't run pip without doing this first.
 sudo xcodebuild -license accept
 
-# Install the latest version of ansible
-pip3 install --user --upgrade ansible
+# Install the latest version of pip. Some older versions won't download cryptography wheels for some reason, causing
+# the ansible install to fail.
+pip3 install --user --upgrade pip
 
-# Install additional python requirements needed by tasks used in these playbooks.
-pip3 install --user --upgrade -r r"${DIR}"/requirements.txt
+# Install ansible and python requirements needed by tasks used in these playbooks.
+pip3 install --user --upgrade -r "${DIR}"/requirements.txt
 
-# Install playbook requirements.
+# Install playbook requirements from ansible galaxy.
 "$(python3 -m site --user-base)"/bin/ansible-galaxy install -r "${DIR}"/requirements.yml
