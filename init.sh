121b152fc6a1cf70ce229f243ba356eda07e94af
Mike Conley <conleym@users.noreply.github.com>
2022-09-16 20:11:11 -0400

    Turns out init might have to run the xcode firstrun tasks if xcode has been upgraded.

--- init.sh
+++ init.sh
@@ -6,6 +6,12 @@ DIR="$( cd "$( dirname "${SCRIPT}" )" >/dev/null 2>&1 && pwd )"
 # Accept the Xcode license, install python and ansible-galaxy dependencies, including ansible itself.
 # Can be run again to upgrade any of the python or ansible-galaxy dependencies.
 
+if ! xcodebuild -checkFirstLaunchStatus; then
+  echo "Running Xcode first launch tasks."
+  sudo xcodebuild -runFirstLaunch
+  echo "Done."
+fi
+
 # Accept the Xcode license. Can't run pip without doing this first.
 sudo xcodebuild -license accept
 
@@ -14,7 +20,7 @@ sudo xcodebuild -license accept
 pip3 install --user --upgrade pip
 
 # Install ansible and python requirements needed by tasks used in these playbooks.
-pip3 install --user --upgrade -r "${DIR}"/requirements.txt
+pip3 install --user --upgrade -r "${DIR}"/requirements.txt --no-warn-script-location
 
 # Install playbook requirements from ansible galaxy.
 "$(python3 -m site --user-base)"/bin/ansible-galaxy install -r "${DIR}"/requirements.yml
