6f1d918ab12a09029760ebe50bf2269196975b0e
Mike Conley <conleym@users.noreply.github.com>
2023-09-24 11:45:37 -0400

    Add a script to reinstall the Xcode command line tools.
    Because Apple sucks.
    
    See https://trac.macports.org/wiki/ProblemHotlist#reinstall-clt

---
+++ reinstall-clt.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env zsh
+
+# Reinstall the Xcode command-line tools. See:
+# https://trac.macports.org/wiki/ProblemHotlist#reinstall-clt
+
+VERSION=$(pkgutil --pkg-info=com.apple.pkg.CLTools_Executables 2>/dev/null | sed -n 's/^version: //p')
+
+IN_PROGRESS_FILE="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+
+if [ -z "${VERSION}" ]; then
+    echo "Command line tools package receipt is missing."
+
+    touch ${IN_PROGRESS_FILE}
+    softwareupdate -l
+
+    echo "You now need to run software update to reinstall the Xcode command line tools. Then run this script again."
+    exit 0
+fi
+
+
+if [ -e "${IN_PROGRESS_FILE}" ]; then
+    echo "Removing in-progress file."
+    
+    rm "${IN_PROGRESS_FILE}"
+    softwareupdate -l
+
+    exit 0
+fi
+
+echo "Everything looks OK. Nothing to do."
